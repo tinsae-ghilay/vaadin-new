8a9a7ccfc8065ced7090a27221ed510f1411256b
Tinsae <tinsaekahsay@gmail.com>
2025-05-04 13:17:18 +0200

    pwa is back

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,5 +1,6 @@
 package com.example.application;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
@@ -18,13 +19,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 // this program will be installable web app. and we can customise its look and feel
 // name of the app, shortName = name that appears below the icon, and offlineResources would be cached
 // we will probably have to add that logo image, but let's set it to what exists for now
-/*@PWA(
+@NpmPackage(value = "line-awesome",version = "1.3.0")
+@PWA(
         name = "Vaadin CRM",
         shortName = "CRM"
         //offlinePath="offline.html",
         //offlineResources = { "./images/offline.png"}
-)*/
-//@NpmPackage(value = "line-awesome",version = "1.3.0")
+)
 
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
